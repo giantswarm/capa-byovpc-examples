68e9f4edf0fdbaadb0e04fced869bcbcd2b3cb3f
iuri aranda <iuri@giantswarm.io>
2024-07-31 17:34:01 +0200

    Restructure examples

--- vpc-examples/public-vpc/outputs.tf
+++ vpc-examples/public-vpc/outputs.tf
@@ -2,23 +2,12 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
-output "public_subnets" {
-  value = [for s in aws_subnet.public : {
-    id             = s.id
-    route_table_id = aws_route_table.public.id
-    nat_gateway_id = aws_nat_gateway.this[s.availability_zone].id
-  }]
-}
-
-output "private_subnets" {
-  value = [for s in aws_subnet.private : {
-    id             = s.id
-    route_table_id = aws_route_table.private[s.availability_zone].id
-  }]
-}
-
 output "internet_gateway_id" {
   value = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_ids" {
+  value = [for ng in aws_nat_gateway.this : ng.id]
 }
 
 output "connectivity_config" {
